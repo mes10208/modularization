14da7ff44be2a1056c948c5f8b92399d66c15620
Daniela Mesalles <amesalles@tigo.com.gt>
2020-07-06 21:07:13 -0600

    Modularización de jupyter notebook

---
+++ src/features/dictionary.py
@@ -0,0 +1,12 @@
+from typing import List
+
+import gensim.corpora as corpora
+from gensim.corpora import Dictionary
+
+
+def create_dictionary(data_lemmatized: List[List[str]]):
+    return corpora.Dictionary(data_lemmatized)
+
+
+def term_document_matrix(texts, dictionary: Dictionary):
+    return [dictionary.doc2bow(text) for text in texts]
---
+++ src/models/predict.py
@@ -0,0 +1,14 @@
+import gensim
+
+def predict(tokens: List[str]):
+	lda_model = gensim.models.ldamodel.LdaModel.load("../../data/models/lda_model.pkl")
+
+	predictions = []
+	for token in tokens:
+		prediction = {
+			'id': token,
+			'topics': lda_model.get_term_topics(token)
+		}
+		prediction.append(prediction)
+
+	return predictions
